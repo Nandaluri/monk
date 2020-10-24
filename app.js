e0d37faf4f9973d966f66c4de7b82e2bea31bc74
Nandaluri <52715127+Nandaluri@users.noreply.github.com>
2020-10-24 20:06:02 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const bg = document.querySelector("#bg");
+const mountain = document.querySelector("#mountain");
+const text = document.querySelector("#texth2");
+const monk = document.querySelector("#monk");
+
+window.addEventListener("scroll", function () {
+  var value = window.scrollY;
+  bg.style.transform = "translateY(" + value + "px)";
+  mountain.style.transform = "translateY(" + value * 0.7 + "px)";
+  text.style.transform = "translateY(" + value * 1.1 + "px)";
+  monk.style.transform = "translateY(" + value * 0.2 + "px)";
+});
