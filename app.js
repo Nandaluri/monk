f3b875b1a85b261b560d67a906b78cf20b16fbff
Nandaluri <52715127+Nandaluri@users.noreply.github.com>
2020-10-25 14:00:55 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const mountain = document.querySelector("#mountain");
 const text = document.querySelector("#texth2");
 const monk = document.querySelector("#monk");
 
-window.addEventListener("scroll", function () {
+window.addEventListener("scroll", function (e) {
+  console.log(e);
   var value = window.scrollY;
   bg.style.transform = "translateY(" + value + "px)";
   mountain.style.transform = "translateY(" + value * 0.7 + "px)";
