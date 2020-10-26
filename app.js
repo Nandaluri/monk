c0aa573ba6863cd27a9b162fc400d6e3d50b5341
Daniel Nanda <danielnanda98@gmail.com>
2020-10-26 09:55:38 +0100

    style and hamburger

--- app.js
+++ app.js
@@ -2,12 +2,22 @@ const bg = document.querySelector("#bg");
 const mountain = document.querySelector("#mountain");
 const text = document.querySelector("#texth2");
 const monk = document.querySelector("#monk");
+const hamburger = document.querySelector(".hamburger");
+const list = document.querySelector(".list");
+const close = document.querySelector(".item4");
+
+hamburger.addEventListener("click", () => {
+  list.style.display = "grid";
+});
+close.addEventListener("click", () => {
+  list.style.display = "none";
+});
 
 window.addEventListener("scroll", function (e) {
   console.log(e);
   var value = window.scrollY;
   bg.style.transform = "translateY(" + value + "px)";
   mountain.style.transform = "translateY(" + value * 0.7 + "px)";
-  text.style.transform = "translateY(" + value * 1.1 + "px)";
+  //text.style.transform = "translateY(" + value * 1.1 + "px)";
   monk.style.transform = "translateY(" + value * 0.2 + "px)";
 });
