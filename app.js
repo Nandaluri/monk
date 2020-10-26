d8b50c83f998e65183d07bf053cf10403e536c88
Daniel Nanda <danielnanda98@gmail.com>
2020-10-27 00:23:05 +0100

    fixes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const monk = document.querySelector("#monk");
 const hamburger = document.querySelector(".hamburger");
 const list = document.querySelector(".list");
 const close = document.querySelector(".item4");
+const nav = document.querySelector("#nav");
 
 hamburger.addEventListener("click", () => {
   list.style.transform = "translateY(0%)";
@@ -13,9 +14,13 @@ close.addEventListener("click", () => {
   list.style.transform = "translateY(-100%)";
 });
 
-window.addEventListener("scroll", function (e) {
-  console.log(e);
+window.addEventListener("scroll", function () {
   var value = window.scrollY;
+  if (value > 0) {
+    nav.style.backgroundColor = "rgba(30, 30, 30, 1)";
+  } else if (value === 0) {
+    nav.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
+  }
   bg.style.transform = "translateY(" + value + "px)";
   mountain.style.transform = "translateY(" + value * 0.7 + "px)";
   //text.style.transform = "translateY(" + value * 1.1 + "px)";
